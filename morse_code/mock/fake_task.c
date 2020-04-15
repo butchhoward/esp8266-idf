322213b357142cb0ccf3e63fe9af3dfca6db8917
Butch Howard <butch.howard@accenture.com>
2020-04-15 10:06:51 -0400

    Remove fake stuff that was not really needed
    The original concern was that the system includes would drag in
    hardware specific stuff. The RTOS layer does not seem to do that.
    
    Also, now that the testing has mock (fff.h), there is more flexibility
    in getting around those needs as well.

--- morse_code/mock/fake_task.c
+++ morse_code/mock/fake_task.c
@@ -1,36 +1,6 @@
-#include "fake_task.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <string.h>
 
-struct xTaskCreate_call xtaskcreate_call;
-TickType_t vtaskdelay_call = 0;
-
-#ifdef xTaskCreatePinnedToCore
-    #undef xTaskCreatePinnedToCore
-#endif
-BaseType_t xTaskCreatePinnedToCore(TaskFunction_t pvTaskCode,
-                                   const char *const pcName,
-                                   const uint32_t usStackDepth,
-                                   void *const pvParameters,
-                                   UBaseType_t uxPriority,
-                                   TaskHandle_t *const pvCreatedTask,
-                                   const BaseType_t xCoreID) {
-  xtaskcreate_call.pvTaskCode = pvTaskCode;
-  xtaskcreate_call.constpcName = pcName;
-  xtaskcreate_call.usStackDepth = usStackDepth;
-  xtaskcreate_call.constpvParameters = pvParameters;
-  xtaskcreate_call.uxPriority = uxPriority;
-  xtaskcreate_call.constpvCreatedTask = pvCreatedTask;
-  return pdPASS;
+void vTaskDelay(const TickType_t xTicksToDelay) PRIVILEGED_FUNCTION 
+{
 }
-
-struct xTaskCreate_call *xTaskCreateCall(void) {
-  return &xtaskcreate_call;
-}
-
-void vTaskDelay(const TickType_t xTicksToDelay) PRIVILEGED_FUNCTION {
-  vtaskdelay_call = xTicksToDelay;
-}
-
-TickType_t vTaskDelayCall() { return vtaskdelay_call; }
