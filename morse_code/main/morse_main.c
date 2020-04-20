534afebb3fb97030e70ff921c076f8432c02c90d
Butch Howard <butch.howard@accenture.com>
2020-04-20 11:59:17 -0400

    Use freertos tasks
    Both led send morse
    they appear to be flashing mostly in sync
    it is not clear to me whether main exits

--- morse_code/main/morse_main.c
+++ morse_code/main/morse_main.c
@@ -6,37 +6,31 @@
 #include "leds.h"
 #include "morse.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void morse_sos(void* morse_config)
+
+static void morse_task(void *morse_config)
 {
     char sos[] = "SOS";
-    morse_word(morse_config, sos);
+    for (;;)
+    {
+        morse_word(morse_config, sos);
+    }
 }
+
 
 void app_main()
 {
-    void* led_config = leds_setup(LEDS_BUILTIN_RED);
-    void* morse_config = morse_setup(led_config);
+    void* led_red_config = leds_setup(LEDS_BUILTIN_RED);
+    void* morse_red_config = morse_setup(led_red_config);
 
     void* led_blue_config = leds_setup(LEDS_BUILTIN_BLUE);
     void* morse_blue_config = morse_setup(led_blue_config);
 
-    bool use_red=true;
+    xTaskCreate(morse_task, "sos_red", 2048, morse_red_config, 10, NULL);
+    xTaskCreate(morse_task, "sos_blue", 2048, morse_blue_config, 10, NULL);
 
-    while(true)
-    {
-        if (use_red)
-        {
-            morse_sos(morse_config);
-        }
-        else
-        {
-            morse_sos(morse_blue_config);
-        }
-
-        use_red = !use_red;
-    }
-
-    morse_cleanup(morse_config);
     morse_cleanup(morse_blue_config);
+    morse_cleanup(morse_red_config);
 }
