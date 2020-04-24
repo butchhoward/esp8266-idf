88e2941f5f0aaf248d8b4b5af1774e949755a79a
Butch Howard <butch.howard@accenture.com>
2020-04-24 16:16:18 -0400

    Copy the example simple http server and get it building

---
+++ internet_button/components/include/connect.h
@@ -0,0 +1,7 @@
+#ifndef CONNECT_H
+#define CONNECT_H
+
+esp_err_t button_server_connect(void);
+
+#endif
+
--- internet_button/main/main.c
+++ internet_button/main/main.c
@@ -1,113 +1,244 @@
 /* Internet Button
 */
 
-#include <stdio.h>
-#include <stdbool.h>
-#include "leds.h"
-#include "relay.h"
-#include "morse.h"
-#include "gpio_assistant.h"
+#include <sys/param.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include <driver/gpio.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "connect.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 
+#include <esp_http_server.h>
 
-SemaphoreHandle_t led_semaphore = NULL;
-void configure_the_semaphore()
+static const char *TAG="APP";
+
+/* An HTTP GET handler */
+esp_err_t hello_get_handler(httpd_req_t *req)
 {
-    led_semaphore =  xSemaphoreCreateBinary();
-    if (led_semaphore == NULL)
-    {
-        printf("semaphore creation failed\n");
-        return;
+    char*  buf;
+    size_t buf_len;
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+        free(buf);
     }
-    //Semaphore has to be given before it can be taken. Whichever takes first gets it.
-    xSemaphoreGive( led_semaphore );
+
+    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-2") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Test-Header-2: %s", buf);
+        }
+        free(buf);
+    }
+
+    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-1") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Test-Header-1: %s", buf);
+        }
+        free(buf);
+    }
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query => %s", buf);
+            char param[32];
+            /* Get value of expected key from query string */
+            if (httpd_query_key_value(buf, "query1", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
+            }
+            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => query3=%s", param);
+            }
+            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => query2=%s", param);
+            }
+        }
+        free(buf);
+    }
+
+    /* Set some custom headers */
+    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
+    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    const char* resp_str = (const char*) req->user_ctx;
+    httpd_resp_send(req, resp_str, strlen(resp_str));
+
+    /* After sending the HTTP response the old HTTP request
+     * headers are lost. Check if HTTP request headers can be read now. */
+    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+        ESP_LOGI(TAG, "Request headers lost");
+    }
+    return ESP_OK;
 }
 
+httpd_uri_t hello = {
+    .uri       = "/hello",
+    .method    = HTTP_GET,
+    .handler   = hello_get_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+    .user_ctx  = "Hello World!"
+};
 
-#define DEVICE_TYPE_FEATHER
-// #define DEVICE_TYPE_WAVESHARE
-
-#if defined(DEVICE_TYPE_WAVESHARE)
-    #define LEDS_BUILTIN_RED LEDS_GPIO_16
-    #define LEDS_BUILTIN_BLUE LEDS_GPIO_2
-#elif defined(DEVICE_TYPE_FEATHER)
-    #define LEDS_BUILTIN_RED LEDS_GPIO_0
-    #define LEDS_BUILTIN_BLUE LEDS_GPIO_2
-#else
-    #error "Use a known DEVICE_TYPE_XXX"
-#endif
-
-#define RELAY_GPIO GPIO_NUM_12
-
-
-void configure_the_gpio_to_use()
+/* An HTTP POST handler */
+esp_err_t echo_post_handler(httpd_req_t *req)
 {
-    //must configure IO before using it (the built in leds worked by accident)
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = gpio_build_pin_select_mask(3, RELAY_GPIO, LEDS_BUILTIN_RED, LEDS_BUILTIN_BLUE);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    if (gpio_config(&io_conf) != ESP_OK)
-    {
-        printf("gpio_config failed\n");
+    char buf[100];
+    int ret, remaining = req->content_len;
+
+    while (remaining > 0) {
+        /* Read the data for the request */
+        if ((ret = httpd_req_recv(req, buf,
+                        MIN(remaining, sizeof(buf)))) <= 0) {
+            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+                /* Retry receiving if timeout occurred */
+                continue;
+            }
+            return ESP_FAIL;
+        }
+
+        /* Send back the same data */
+        httpd_resp_send_chunk(req, buf, ret);
+        remaining -= ret;
+
+        /* Log data received */
+        ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
+        ESP_LOGI(TAG, "%.*s", ret, buf);
+        ESP_LOGI(TAG, "====================================");
+    }
+
+    // End response
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
+}
+
+httpd_uri_t echo = {
+    .uri       = "/echo",
+    .method    = HTTP_POST,
+    .handler   = echo_post_handler,
+    .user_ctx  = NULL
+};
+
+/* An HTTP PUT handler. This demonstrates realtime
+ * registration and deregistration of URI handlers
+ */
+esp_err_t ctrl_put_handler(httpd_req_t *req)
+{
+    char buf;
+    int ret;
+
+    if ((ret = httpd_req_recv(req, &buf, 1)) <= 0) {
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            httpd_resp_send_408(req);
+        }
+        return ESP_FAIL;
+    }
+
+    if (buf == '0') {
+        /* Handler can be unregistered using the uri string */
+        ESP_LOGI(TAG, "Unregistering /hello and /echo URIs");
+        httpd_unregister_uri(req->handle, "/hello");
+        httpd_unregister_uri(req->handle, "/echo");
+    }
+    else {
+        ESP_LOGI(TAG, "Registering /hello and /echo URIs");
+        httpd_register_uri_handler(req->handle, &hello);
+        httpd_register_uri_handler(req->handle, &echo);
+    }
+
+    /* Respond with empty body */
+    httpd_resp_send(req, NULL, 0);
+    return ESP_OK;
+}
+
+httpd_uri_t ctrl = {
+    .uri       = "/ctrl",
+    .method    = HTTP_PUT,
+    .handler   = ctrl_put_handler,
+    .user_ctx  = NULL
+};
+
+httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &hello);
+        httpd_register_uri_handler(server, &echo);
+        httpd_register_uri_handler(server, &ctrl);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+void stop_webserver(httpd_handle_t server)
+{
+    // Stop the httpd server
+    httpd_stop(server);
+}
+
+static httpd_handle_t server = NULL;
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base, 
+                               int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server) {
+        ESP_LOGI(TAG, "Stopping webserver");
+        stop_webserver(*server);
+        *server = NULL;
     }
 }
 
-
-void* relay_config = 0;
-
-static void toggle_relay()
+static void connect_handler(void* arg, esp_event_base_t event_base, 
+                            int32_t event_id, void* event_data)
 {
-    static bool relay_flag = false;
-
-    if (relay_flag)
-    {
-        relay_on(relay_config);
-    }
-    else 
-    {
-        relay_off(relay_config);
-    }
-    relay_flag = !relay_flag;
-}
-
-static void morse_task(void *morse_config)
-{
-    char sos[] = "SOS";
-    for (;;)
-    {
-        //wait (possibly forever) for the semaphore to become available
-        xSemaphoreTake( led_semaphore, portMAX_DELAY );
-        toggle_relay();
-        morse_word(morse_config, sos);
-        xSemaphoreGive( led_semaphore );
-
-        //must yield/delay in some way to give the task manager time to go to another task
-        taskYIELD();
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server == NULL) {
+        ESP_LOGI(TAG, "Starting webserver");
+        *server = start_webserver();
     }
 }
-
-
 
 void app_main()
 {
-    printf("start of main\n");
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    configure_the_semaphore();
-    configure_the_gpio_to_use();
 
-    relay_config = relay_setup_mode(RELAY_GPIO, RELAY_ACTIVE_HIGH);
-    void* morse_red_config = morse_setup_options( leds_setup(LEDS_BUILTIN_RED), 50);
-    void* morse_blue_config = morse_setup_options( leds_setup(LEDS_BUILTIN_BLUE), 100) ;
+    ESP_ERROR_CHECK(button_server_connect());
 
-    xTaskCreate(morse_task, "sos_red", 2048, morse_red_config, 10, NULL);
-    xTaskCreate(morse_task, "sos_blue", 2048, morse_blue_config, 10, NULL);
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
 
-    printf("end of main\n");
+    server = start_webserver();
 }
